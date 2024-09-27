379d7562b1ff32c26e446f523d51ffc5d1b1c59d
Samuel Tessema <stessema@salesforce.com>
2024-09-26 20:32:04 -0700

    boilerplate server code

---
+++ main.go
@@ -0,0 +1,23 @@
+package main 
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Received a request to '/'")
+		w.Write([]byte("Welcome to the Liberty Hills app!"))
+	})
+
+	// serve traffic
+	slog.Info("Server starting up...")
+	http.ListenAndServe(":3000", r)
+	slog.Info("Server exited!")
+}
